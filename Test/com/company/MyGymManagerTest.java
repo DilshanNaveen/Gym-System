fc79deaf58a1d56a081749136c755c00d526a02b
Dilshan Naveen <dilshannaveen20@hotmail.com>
2020-08-03 03:39:58 +0530

    creating test

---
+++ Test/com/company/MyGymManagerTest.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MyGymManagerTest {
+
+    @Test
+    public void addMember() {
+
+    }
+
+    @Test
+    public void deleteMember() {
+    }
+
+    @Test
+    public void printListMember() {
+    }
+
+    @Test
+    public void sortList() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void loadTable() {
+    }
+
+    @Test
+    public void searchTableById() {
+    }
+
+    @Test
+    public void loadMemberList() {
+    }
+}
